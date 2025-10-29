eb7919b77e660151cc5d17b31ac5536173311b61
Simon Manzler <simanzler@gmail.com>
2025-10-28 21:01:41 -0400

    added theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,23 @@ import {
 } from "./components/ui/sidebar";
 import { AppSidebar } from "./components/app-sidebar";
 import Index from "./components/pages/index";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
-    <SidebarProvider defaultOpen={false}>
-      <AppSidebar variant="floating" />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
-          <SidebarTrigger />
-        </header>
-        <Index />
-      </SidebarInset>
-    </SidebarProvider>
+    <ThemeProvider>
+      <SidebarProvider defaultOpen={false}>
+        <AppSidebar variant="floating" />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+            <SidebarTrigger />
+            <ModeToggle className="ml-auto" variant="ghost" />
+          </header>
+          <Index />
+        </SidebarInset>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
 
