057db6ce58b430ff86ec6f0128a2bffc65f6a19b
Simon Manzler <simanzler@gmail.com>
2025-11-17 17:48:08 -0500

    updated schema

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -25,37 +25,10 @@ const db = new Dexie("WrittenDatabase") as Dexie & {
 };
 
 db.version(1).stores({
-  journals: "++id, title, content, createdAt, updatedAt",
+  journals:
+    "++id, is_encrypted, raw_blob, encrypted_blob, created_at, updated_at",
   settings: "id",
 });
-
-db.version(2)
-  .stores({
-    journals:
-      "++id, is_encrypted, raw_blob, encrypted_blob, created_at, updated_at",
-    settings: "id",
-  })
-  .upgrade(async (tx) => {
-    const journals = await tx.table("journals").toArray();
-
-    for (const journal of journals) {
-      const rawContent = journal.raw_blob;
-
-      try {
-        const parsed = JSON.parse(rawContent);
-        if (parsed && parsed.ciphertext) {
-          journal.is_encrypted = true;
-          journal.encrypted_blob = rawContent;
-        } else {
-          journal.raw_blob = rawContent;
-        }
-      } catch (e) {
-        journal.raw_blob = rawContent;
-      }
-
-      await tx.table("journals").update(journal.id, journal);
-    }
-  });
 
 export type { Journal, Settings };
 export { db };
